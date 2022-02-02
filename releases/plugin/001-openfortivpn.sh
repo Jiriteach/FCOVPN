cb5d09bc0d69f569c394192e016e25e08d5d8076
Jithen (J) Singh <jxs@eagle.co.nz>
2022-02-03 08:08:14 +1300

    Refactoring

--- releases/plugin/001-openfortivpn.sh
+++ releases/plugin/001-openfortivpn.sh
@@ -1,33 +1,34 @@
 #!/bin/bash
 
-# Get current status of a OpenFortiVPN connection with options to connect/disconnect.
-# Commands that require admin permissions should be whitelisted with 'visudo', e.g.:
+# Commands that require admin permissions should be whitelisted with 'visudo' - example:
 # YOURUSERNAME ALL=(ALL) NOPASSWD: /usr/local/bin/openfortivpn
 # YOURUSERNAME ALL=(ALL) NOPASSWD: /usr/bin/killall -2 openfortivpn
+
+# Configuration for openfortivpn
 # To use openfortivpn in an easy way you can create file like: /Documents/.fortivpn-config and put your crential in it as following:
 #
 # host=123.45.678.9
 # port=1234
-# username=FOO
-# password=MYPASSWORDCHARACHTERS
-# trusted-cert=MYCERTIFICATECHARACHTERS
+# username=username
+# password=password
+# trusted-cert=certificate
 
-# <xbar.title>OpenFortiVPN</xbar.title>
+# <xbar.title>FCOVPN</xbar.title>
 # <xbar.version>v1.0</xbar.version>
-# <xbar.author>Emran Mobaraki Novin</xbar.author>
-# <xbar.author.github>emranovin</xbar.author.github>
-# <xbar.desc>Displays status of a OpenFortiVPN interface with option to connect/disconnect.</xbar.desc>
+# <xbar.author>Original - Emran Mobaraki Novin. Customised - Jithen Singh</xbar.author>
+# <xbar.author.github>Original - emranovin. Customised - jiriteach</xbar.author.github>
+# <xbar.desc>FCOVPN</xbar.desc>
 # <xbar.image>https://i.imgur.com/v2aW5mo.png</xbar.image>
 
-VPN_EXECUTABLE=/usr/local/bin/openfortivpn
-VPN_EXECUTABLE_PARAMS="-c$HOME/Documents/.fortivpn-config"
+VPN_EXECUTABLE=/usr/local/bin/openfortivpn #location of openfortivpn
+VPN_EXECUTABLE_PARAMS="-c$HOME/Documents/.fortivpn-config" #location of openfortivpn configuration file
 
-VPN_INTERFACE=ppp0
+VPN_INTERFACE=ppp0 #vpn interface on MacOS
 
 VPN_CONNECTED="/sbin/ifconfig | egrep -A1 $VPN_INTERFACE | grep inet"
 VPN_DISCONNECT_CMD="sudo killall -2 openfortivpn"
 
-EXTERNAL_URL="https://"
+EXTERNAL_URL="https://" #url of VPN server
 
 INTERNAL_IP_ADDRESSES="ifconfig | grep 'inet ' | grep -v 127.0.0.1 | cut -d\  -f2"
 EXTERNAL_IP_ADDRESSES="curl checkip.amazonaws.com"
@@ -37,16 +38,13 @@ TEST_PING="./sbin/ping 1.1.1.1"
 
 case "$1" in
     connect)
-        # VPN connection command, should eventually result in $VPN_CONNECTED,
-        #sudo "$VPN_EXECUTABLE" "$VPN_EXECUTABLE_PARAMS" &> /dev/null &
         sudo "$VPN_EXECUTABLE" &> /dev/null &
 
-        # Wait for connection so menu item refreshes instantly
         until eval "$VPN_CONNECTED"; do sleep 1; done
         ;;
     disconnect)
         eval "$VPN_DISCONNECT_CMD"
-        # Wait for disconnection so menu item refreshes instantly
+
         until [ -z "$(eval "$VPN_CONNECTED")" ]; do sleep 1; done
         ;;
 esac
@@ -63,7 +61,7 @@ if [ -n "$(eval "$VPN_CONNECTED")" ]; then
     echo "External Address - "
     echo "$(eval "$EXTERNAL_IP_ADDRESSES")"
     echo "---"
-    echo "Ping Internal Server | shell='ping' param1='-c4 172.27.16.204' terminal=true"
+    echo "Ping Internal Server | shell='ping' param1='-c4 172.27.16.204' terminal=true" #IP address on the VPN network to test connectivity
     echo "Refresh ... | refresh=true"
     exit
 else
